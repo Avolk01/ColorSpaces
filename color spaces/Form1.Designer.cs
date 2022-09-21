5757a5fa174cf0bcdbcea0ba80c1c9bc6e520405
AVolk <volkodavartem01@mail.ru>
2022-09-21 23:00:00 +0300

    Добавьте файлы проекта.

---
+++ color spaces/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace color_spaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.greyscaleBox1 = new System.Windows.Forms.PictureBox();
+            this.greyscaleBox2 = new System.Windows.Forms.PictureBox();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.greyscaleBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyscaleBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawButton
+            // 
+            this.drawButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawButton.Location = new System.Drawing.Point(1058, 666);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 0;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mainPictureBox);
+            this.panel1.Controls.Add(this.greyscaleBox2);
+            this.panel1.Controls.Add(this.greyscaleBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1052, 702);
+            this.panel1.TabIndex = 1;
+            // 
+            // greyscaleBox1
+            // 
+            this.greyscaleBox1.Location = new System.Drawing.Point(12, 12);
+            this.greyscaleBox1.Name = "greyscaleBox1";
+            this.greyscaleBox1.Size = new System.Drawing.Size(484, 312);
+            this.greyscaleBox1.TabIndex = 0;
+            this.greyscaleBox1.TabStop = false;
+            // 
+            // greyscaleBox2
+            // 
+            this.greyscaleBox2.Location = new System.Drawing.Point(559, 12);
+            this.greyscaleBox2.Name = "greyscaleBox2";
+            this.greyscaleBox2.Size = new System.Drawing.Size(484, 312);
+            this.greyscaleBox2.TabIndex = 1;
+            this.greyscaleBox2.TabStop = false;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(284, 377);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(484, 312);
+            this.mainPictureBox.TabIndex = 2;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 701);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.drawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.greyscaleBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greyscaleBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.PictureBox greyscaleBox2;
+        private System.Windows.Forms.PictureBox greyscaleBox1;
+    }
+}
+
---
+++ color spaces/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace color_spaces
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+
+            Bitmap mainPicture = new Bitmap(Image.FromFile("test1.png"));
+            this.mainPictureBox.Image = mainPicture;
+            Bitmap greyscale1 = new Bitmap(mainPicture);
+            Bitmap greyscale2 = new Bitmap(mainPicture);
+            int x, y;
+
+            // Loop through the images pixels to reset color.
+            for (x = 0; x < mainPicture.Width; x++)
+            {
+                for (y = 0; y < mainPicture.Height; y++)
+                {
+                    Color pixelColor = mainPicture.GetPixel(x, y);
+                    var value1 = pixelColor.R * 0.299f + pixelColor.G * 0.587f + pixelColor.B * 0.114f;
+                    var value2 = pixelColor.R * 0.2126f + pixelColor.G * 0.7152f + pixelColor.B * 0.0722f;
+                    Color newColor1 = Color.FromArgb((int)value1, (int)value1, (int)value1);
+                    Color newColor2 = Color.FromArgb((int)value1, (int)value1, (int)value1);
+                    greyscale1.SetPixel(x, y, newColor1); 
+                    greyscale2.SetPixel(x, y, newColor1); 
+                }
+            }            
+            greyscaleBox1.Image = greyscale1;
+            greyscaleBox2.Image = greyscale2;
+        }
+    }
+}
